eb57e1b8dc63147020af4930f44148abd84ef392
Ding Harley Catubag <dingharley123@gmail.com>
2025-01-24 16:21:06 +0800

    adding project page

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,26 +1,18 @@
-// import DecorationBar from "@/components/DecorationBar";
-// import { GoArrowDownRight } from "react-icons/go";
-
-// const page = () => {
-//   return (
-//     <main className="my-20 md:my-28 max-xl:px-6">
-//       <DecorationBar />
-//       <div className="flex flex-row items-center gap-4 relative left-[-5px] mt-2">
-//         <GoArrowDownRight size={25} />
-//         <p className="tracking-wider text-xl leading-[1.2] font-bold">
-//           Selected Works
-//         </p>
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default page;
-
-import React from "react";
+import { GoArrowDownRight } from "react-icons/go";
+import DecorationBar from "@/components/DecorationBar";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <main className="my-20 md:my-28 max-xl:px-6">
+      <DecorationBar />
+      <div className="flex flex-row items-center gap-4 relative left-[-5px] mt-2">
+        <GoArrowDownRight size={25} />
+        <p className="tracking-wider text-xl leading-[1.2] font-bold">
+          Selected Works
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default page;
