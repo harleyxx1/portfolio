1e68e1e7387c5bf4395beac8903e8060f8ce24a6
Ding Harley Catubag <dingharley123@gmail.com>
2025-01-24 15:55:14 +0800

    changing pages

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,18 +1,26 @@
-import DecorationBar from "@/components/DecorationBar";
-import { GoArrowDownRight } from "react-icons/go";
+// import DecorationBar from "@/components/DecorationBar";
+// import { GoArrowDownRight } from "react-icons/go";
+
+// const page = () => {
+//   return (
+//     <main className="my-20 md:my-28 max-xl:px-6">
+//       <DecorationBar />
+//       <div className="flex flex-row items-center gap-4 relative left-[-5px] mt-2">
+//         <GoArrowDownRight size={25} />
+//         <p className="tracking-wider text-xl leading-[1.2] font-bold">
+//           Selected Works
+//         </p>
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default page;
+
+import React from "react";
 
 const page = () => {
-  return (
-    <main className="my-20 md:my-28 max-xl:px-6">
-      <DecorationBar />
-      <div className="flex flex-row items-center gap-4 relative left-[-5px] mt-2">
-        <GoArrowDownRight size={25} />
-        <p className="tracking-wider text-xl leading-[1.2] font-bold">
-          Selected Works
-        </p>
-      </div>
-    </main>
-  );
+  return <div>page</div>;
 };
 
 export default page;
